e177dc7b7acdc727a9ad439b4bc68993739cf508
Luke Shimkus <super99leo@gmail.com>
2014-10-31 11:33:26 -0400

    Update alt-tab.el

--- alt-tab.el
+++ alt-tab.el
@@ -1,3 +1,24 @@
+;; Usuful buffer elisp functions
+
+;; return the name of current buffer
+;;(buffer-name)
+
+;; return the full path of the file
+;;(buffer-file-name)
+
+;; switch to a given buffer
+;;(set-buffer myBuffer)
+
+;; close a given buffer
+;;(kill-buffer myBuffer)
+
+;; use a temp buffer to manipulate string
+;;(with-temp-buffer
+;;  (insert myStr)
+;;   manipulate the string here
+;;  (buffer-string) ; get result
+;;)
+
 ;; Defining the variables
 
 (defvar default-buffer-amount 4 "Default buffer amount wehn loading your Emacs file.")
