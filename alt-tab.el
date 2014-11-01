4f161664446766ffcd7d8928e9f744803e7e6d8a
Luke Shimkus <super99leo@gmail.com>
2014-11-01 11:19:45 -0400

    Update alt-tab.el

--- alt-tab.el
+++ alt-tab.el
@@ -1,6 +1,41 @@
 ;; Welcome to Alt-Tab
 ;; Created by: Luke Shimkus
 
+;; Sessions
+  
+;; use only one desktop
+(setq desktop-path '("~/.emacs.d/"))
+(setq desktop-dirname "~/.emacs.d/")
+(setq desktop-base-file-name "workflow-desktop")
+
+;; remove desktop after it's been read
+(add-hook 'desktop-after-read-hook
+	  '(lambda ()
+	     ;; desktop-remove clears desktop-dirname
+	     (setq desktop-dirname-tmp desktop-dirname)
+	     (setq desktop-dirname desktop-dirname-tmp)))
+
+(defun saved-session ()
+  (file-exists-p (concat desktop-dirname "/" desktop-base-file-name)))
+
+;; use session-restore to restore the desktop manually
+(defun session-restore ()
+  "Restore a saved emacs session."
+  (interactive)
+  (if (saved-session)
+      (desktop-read)
+    (message "No desktop found.")))
+
+;; use session-save to save the desktop manually
+(defun session-save ()
+  "Save an emacs session."
+  (interactive)
+  (if (saved-session)
+      (if (y-or-n-p "Overwrite existing desktop? ")
+	  (desktop-save-in-desktop-dir)
+	(message "Session not saved."))
+    (desktop-save-in-desktop-dir)))
+    
 
 ;; Transposing Buffers
 
