e1fbb3bc1a22c80d145aeb32cbb84b57e0a7d360
Luke Shimkus <super99leo@gmail.com>
2014-11-01 10:37:11 -0400

    Update alt-tab.el

--- alt-tab.el
+++ alt-tab.el
@@ -1,24 +1,14 @@
-;; Usuful buffer elisp functions
+;; Welcome to Alt-Tab
+;; Created by: Luke Shimkus
+(defun transpose-buffers (arg)
+  "Transpose the buffers shown in two windows."
+  (interactive "p")
+  (let ((selector (if (>= arg 0) 'next-window 'previous-window)))
+    (while (/= arg 0)
+      (let ((this-win (window-buffer))
+            (next-win (window-buffer (funcall selector))))
+        (set-window-buffer (selected-window) next-win)
+        (set-window-buffer (funcall selector) this-win)
+        (select-window (funcall selector)))
+      (setq arg (if (plusp arg) (1- arg) (1+ arg))))))
 
-;; return the name of current buffer
-;;(buffer-name)
-
-;; return the full path of the file
-;;(buffer-file-name)
-
-;; switch to a given buffer
-;;(set-buffer myBuffer)
-
-;; close a given buffer
-;;(kill-buffer myBuffer)
-
-;; use a temp buffer to manipulate string
-;;(with-temp-buffer
-;;  (insert myStr)
-;;   manipulate the string here
-;;  (buffer-string) ; get result
-;;)
-
-;; Defining the variables
-
-(defvar default-buffer-amount 4 "Default buffer amount wehn loading your Emacs file.")
